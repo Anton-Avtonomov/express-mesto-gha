c1c583fe97c96766c080604141e0b00c3aabb83e
Anton Avtonomov <antonavtonomov@yandex.ru>
2022-12-20 19:18:33 +0300

    FEAT: add error message

--- controllers/users.js
+++ controllers/users.js
@@ -2,26 +2,39 @@ const Users = require('../models/user');
 
 exports.getUsers = (req, res) => {
   Users.find({})
-    .then((user) => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка!' }));
+    .then((user) => res.send(user))
+    .catch(() => res.status(500).send({ message: 'Запрашиваемый пользователь не найден' }));
 };
 
 exports.getUserById = (req, res) => {
   Users.findById(req.params.userId)
     .then((user) => res.status(200).send(user))
-    .catch(() => {
-      res.status(500).send({ message: 'Запрашиваемый пользователь не найден!' })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(400).send({message: 'Переданы некорректные данные юзера!'})
+      }
+      if (err.message === 'NotFound') {
+        return res.status(404).send({message: 'Пользователь с указанными данными не найден!'})
+      }
+      return res.status(500).send({message: 'Произошла ошибка!'})
     })
 };
 
 exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   Users.create({ name, about, avatar })
+  // user - ответ сервера
     .then((user) => {
       res.status(200).send(user)
     })
-    .catch(() => {
-      res.status(500).send('Произошла ошибка!');
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({message: 'Переданы некорректные данные Юзера!'})
+      }
+      if (err.message === 'NotFound') {
+        return res.status(404).send({message: 'Пользователь с указанными данными уже существует!'})
+      }
+      return res.status(500).send({message: 'Произошла ошибка!'})
     });
 };
 
@@ -30,10 +43,16 @@ exports.updateProfile = (req, res) => {
 
   Users.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
     .then((user) => {
-    res.status(200).send(user);
+     res.status(200).send(user);
   })
-  .catch(() => {
-  res.status(500).send({ message: 'Произошла ошибка!' })
+  .catch((err) => {
+    if (err.name === 'ValidationError') {
+      return res.status(400).send({message: 'Переданы некорректные данные профиля!'})
+    }
+    if (err.message === 'NotFound') {
+      return res.status(404).send({message: 'Пользователь по указанному ID не найден!'})
+    }
+    return res.status(500).send({message: 'Произошла ошибка!'})
   });
 };
 
@@ -45,7 +64,13 @@ exports.updateAvatar = (req, res) => {
     .then((user) => {
       res.status(200).send(user);
     })
-    .catch(() => {
-      res.status(500).send({ message: 'Произошла ошибка!' });
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({message: 'Переданы некорректные данные аватара!'})
+      }
+      if (err.message === 'NotFound') {
+        return res.status(404).send({message: 'Пользователь с указанным ID не найден!'})
+      }
+      return res.status(500).send({message: 'Произошла ошибка!'})
     });
-}
+};
